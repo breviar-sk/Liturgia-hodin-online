7bc21390975644cc2f89c33ee250f1a6826afb57
Richard Kralovic <riso@kralovic.org>
2016-10-22 04:00:07 +0200

    Aktualizacia zdrojakov podla posledneho vyvoja

--- cgi-bin/source/Application.mk
+++
@@ -1,4 +0,0 @@
-#APP_ABI := armeabi mips x86
-#APP_ABI := armeabi mips
-APP_ABI := armeabi
-
--- cgi-bin/source/android.h
+++
@@ -1,9 +0,0 @@
-#include "mysystem.h"
-#include "breviar.h"
-
-#ifdef IO_ANDROID
-
-extern FILE *stdin_pipe;
-
-#endif
-
